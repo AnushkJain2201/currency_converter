fb01483ed142a3c7962cd9c87c572cb44ed7aec0
Anushk Jain <anushk2201@gmail.com>
2024-01-04 18:10:10 +0530

    feat: finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState('EUR');
+  const [to, setTo] = useState('USD');
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    const converter = async () => {
+      const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`);
+
+      const conResult = await response.json();
+
+      const final = conResult['rates'][to];
+
+      setResult((final));
+    }
+
+    if(from === to) {
+      setResult("");
+      return;
+    }
+
+    converter();
+
+  }, [amount, from, to]);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
+
+      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <select value={to} onChange={(e) => setTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <h1>{result}</h1>
     </div>
   );
 }
